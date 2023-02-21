0870b7c658c78e8b1f0409fb046b384096e256d4
Denis Githuku <githukudenis@gmail.com>
2023-02-21 18:12:32 +0300

    Set system bar background and color according to system theme

--- app/src/main/java/com/githukudenis/newsflash/ui/MainActivity.kt
+++ app/src/main/java/com/githukudenis/newsflash/ui/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.githukudenis.newsflash.ui.screens.NavGraphs
 import com.githukudenis.newsflash.ui.theme.NewsFlashTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,26 +21,25 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // Remember a SystemUiController
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = !isSystemInDarkTheme()
+
+            DisposableEffect(systemUiController, useDarkIcons) {
+                // Update all of the system bar colors to be transparent, and use
+                // dark icons if we're in light theme
+                systemUiController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons
+                )
+
+                // setStatusBarColor() and setNavigationBarColor() also exist
+
+                onDispose {}
+            }
             NewsFlashTheme {
                 DestinationsNavHost(navGraph = NavGraphs.root)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(
-    showSystemUi = true,
-    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
-    device = "id:pixel_6_pro"
-)
-@Composable
-fun DefaultPreview() {
-    NewsFlashTheme {
-        Greeting("Android")
     }
 }
